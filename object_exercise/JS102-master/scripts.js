d8f5230839ebb5e952a17773c60d3fccbb677b0e
dn0912 <nguyen.duc0912@googlemail.com>
2017-03-29 15:21:05 +0200

    exercise 1

--- object_exercise/JS102-master/scripts.js
+++ object_exercise/JS102-master/scripts.js
@@ -1,3 +1,20 @@
 //Use this file to implement Part One of your project
 
+var animal = {};
 
+animal.username = 'test';
+console.log(animal.username);
+
+animal['tagline'] = 'test2';
+console.log(animal['tagline']);
+
+var noises = [];
+animal.noises = noises;
+console.log(animal);
+
+var i = 0;
+for(var key in animal){
+	i++;
+	if(key=='username') console.log('Hi my name is '+animal[key]);
+	if(key=='tagline') console.log('Hi my name is '+animal[key]);
+}
