b36faf39ff00774e363d16e7722bf899b1324e9d
dn0912 <nguyen.duc0912@googlemail.com>
2017-03-30 17:03:05 +0200

    exercise closure 5

--- introduction_to_fucntional_js/functionalJS-master/closures/closure.js
+++ introduction_to_fucntional_js/functionalJS-master/closures/closure.js
@@ -29,3 +29,27 @@ var lasNameTrier = function(firstname){
 
 var firstNameFarmer = lasNameTrier('Farmer');
 firstNameFarmer('Brown');
+
+//Create a storyWriter function that returns an object with two methods. One method, addWords adds a word to your story and returns the story while the other one, erase, resets the story back to an empty string. Here is an implementation:
+
+var storyWriter = function(){
+	var story = '';
+	return {
+		addWords: function(string){
+			story = story+string;
+			console.log(story);
+		},
+		erase: function(string){
+			story = '';
+		}
+	};
+};
+
+var farmLoveStory = storyWriter();
+farmLoveStory.addWords('There was once a lonely cow.'); // 'There was once a lonely cow.'
+farmLoveStory.addWords('It saw a friendly face.'); //'There was once a lonely cow. It saw a friendly face.'
+
+var storyOfMyLife = storyWriter();
+storyOfMyLife.addWords('My code broke.'); // 'My code broke.'
+storyOfMyLife.addWords('I ate some ice cream.'); //'My code broke. I ate some ice cream.'
+storyOfMyLife.erase(); // ''
