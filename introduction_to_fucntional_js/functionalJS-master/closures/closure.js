7536b601393b8e4b814496693c3bf7655c74186e
dn0912 <nguyen.duc0912@googlemail.com>
2017-03-30 16:48:41 +0200

    exercise closure 2

--- introduction_to_fucntional_js/functionalJS-master/closures/closure.js
+++ introduction_to_fucntional_js/functionalJS-master/closures/closure.js
@@ -3,8 +3,14 @@
 // In your function, nonsense, change the immediate call to a setTimeout so that the call to blab comes after 2 seconds. The blab function itself should stay the same as before.
 
 var nonsense = function(string){
-	var blab = function()}{
+	var blab = function(){
 		alert(string);
 	};
-	blab();
+	setTimeOut(return blab, 2000);
 };
+
+
+//Now, instead of calling blab inside of nonsense, return blab (without invoking it). Call nonsense with some string and store the returned value (the blab function) in a variable called blabLater. Call nonsense again with a different string and store the returned value in a variable called blabAgainLater.
+
+var blabLater = nonsense('hello');
+var blabAgainLater = nonsense('hi');
